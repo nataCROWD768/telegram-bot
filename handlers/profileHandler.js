0d858a604f6759c869d677df6d5ed1f5176be67d
nataCROWD768 <y3asselikhov@gmail.com>
2025-03-25 21:36:01 +0300

    nataCROWD768BOT 3.5.1

--- handlers/profileHandler.js
+++ handlers/profileHandler.js
@@ -1,34 +1,33 @@
 const Visit = require('../models/visit');
 
 const showProfile = async (bot, msg) => {
-  const chatId = msg.chat.id;
-  const { first_name, last_name, username } = msg.from;
+    const chatId = msg.chat.id;
+    const { first_name, last_name, username } = msg.from;
 
-  try {
-    const visit = await Visit.findOne({ userId: chatId });
-    if (!visit) {
-      await bot.sendMessage(chatId, '❌ Профиль не найден');
-      return;
-    }
+    try {
+        const visit = await Visit.findOne({ userId: chatId });
+        if (!visit) {
+            await bot.sendMessage(chatId, '❌ Профиль не найден');
+            return;
+        }
 
-    // Формируем имя: "Фамилия Имя" или только "Имя", если фамилии нет
-    const fullName = last_name ? `${last_name} ${first_name}` : first_name;
+        // Формируем имя: "Имя Фамилия" или только "Имя", если фамилии нет
+        const fullName = last_name ? `${first_name} ${last_name}` : first_name;
 
-    // Красиво оформленный текст профиля
-    const profileText = `
+        // Красиво оформленный текст профиля
+        const profileText = `
 ✨ **Личный кабинет** ✨
 ═══════════════════════
 👤 **Имя:** _${fullName}_
-🌐 **Ваша реферальная ссылка:**
-[🔗 Присоединяйтесь в клуб Radar GP!](https://my.radargp.com/club/0000046C)
+🔗 https://my.radargp.com/club/0000046C
 ═══════════════════════
-💡 _Используйте ссылку, чтобы пригласить друзей и получить бонусы!_
+💡 _Используйте ссылкудля регистрации и покупок в интернет магазине_
     `.trim();
 
-    await bot.sendMessage(chatId, profileText, { parse_mode: 'Markdown' });
-  } catch (error) {
-    await bot.sendMessage(chatId, '❌ Ошибка');
-  }
+        await bot.sendMessage(chatId, profileText, { parse_mode: 'Markdown' });
+    } catch (error) {
+        await bot.sendMessage(chatId, '❌ Ошибка');
+    }
 };
 
 module.exports = { showProfile };
